07deda37f910d640a691bdefe821a1ba6f0555fd
Usman <ghaniusman0287@gmail.com>
2025-10-26 22:55:03 +0500

    Implement CompanionCard, CompanionList, and CTA components; enhance page layout with popular companions section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,51 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <Button>GO On!</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard 
+          id="123" 
+          name="Neura the brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#E5D0FF"
+          bookmarked={true}/>
+        <CompanionCard 
+          id="321" 
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#FFDA6E"
+          bookmarked={true}/>
+        <CompanionCard 
+          id="543" 
+          name="Verba the Vocabulary Builder"
+          topic="English Literature "
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+          bookmarked={true}/>
+      </section>
+
+      <section className='home-section'>
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
