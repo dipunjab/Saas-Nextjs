261d62610f62e0ef06798ed21519a450a6aac26f
Usman <ghaniusman0287@gmail.com>
2025-11-09 15:35:03 +0500

    refactor: streamline Page component by removing hardcoded CompanionCard instances and utilizing dynamic data

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,37 @@
-import CompanionCard from '@/components/CompanionCard'
-import CompanionList from '@/components/CompanionList'
-import CTA from '@/components/CTA'
-import { Button } from '@/components/ui/button'
-import { recentSessions } from '@/constants'
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.action";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
-      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <h1>Popular Companions</h1>
 
-      <section className='home-section'>
-        <CompanionCard 
-          id="123" 
-          name="Neura the brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#E5D0FF"
-          bookmarked={true}/>
-        <CompanionCard 
-          id="321" 
-          name="Countsy the Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="maths"
-          duration={30}
-          color="#FFDA6E"
-          bookmarked={true}/>
-        <CompanionCard 
-          id="543" 
-          name="Verba the Vocabulary Builder"
-          topic="English Literature "
-          subject="language"
-          duration={30}
-          color="#BDE7FF"
-          bookmarked={true}/>
-      </section>
+        <section className="home-section">
+            {companions.map((companion) => (
+                <CompanionCard
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
+                />
+            ))}
 
-      <section className='home-section'>
-        <CompanionList
-          title="Recently completed sessions"
-          companions={recentSessions}
-          classNames="w-2/3 max-lg:w-full"
-        />
-        <CTA/>
-      </section>
+        </section>
+
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessionsCompanions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA />
+        </section>
     </main>
   )
 }
